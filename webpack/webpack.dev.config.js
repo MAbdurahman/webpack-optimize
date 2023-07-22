7a21f3ca8ea017581accaeaab345842fcea4431d
MAbdurahman <mdbdrrhm@gmail.com>
2023-07-22 07:41:41 -0500

    Set up Webpack Dev Server

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,6 +1,21 @@
 const { merge } = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.config');
 
 module.exports = merge(common, {
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    port: 8000,
+    static: {
+      directory: path.resolve(__dirname, '..'),
+    },
+    devMiddleware: {
+      index: 'index.html',
+      writeToDisk: true,
+    },
+    client: {
+      overlay: true
+    },
+    liveReload: false,
+  }
 });
