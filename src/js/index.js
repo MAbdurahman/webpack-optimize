1a3e2cb8d0d9e21bbef05f83b7ec1b0b981e28f7
MAbdurahman <mdbdrrhm@gmail.com>
2023-08-12 12:17:14 -0500

    Add hamburger and its animation open and close

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,25 @@
 import $ from 'jquery';
 import './../styles/index.scss';
 
-/* ============================================
+/* =============================================
             preloader
-===============================================*/
+================================================*/
 $(window).on('load', function () {
   // makes sure that whole site is loaded
   $('#preloader-gif, #preloader').fadeOut(8000, function() {});
 });
 
+/* =============================================
+            header and navigation
+================================================*/
 $(function () {
   console.log('document is ready!');
+  const navbar_navigation = document.querySelector('.header__navbar__contents__nav');
+  const hamburger = document.querySelector('.header__navbar__contents__hamburger');
+  console.log(navbar_navigation)
+  console.log(hamburger)
+
+  hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('header__navbar__contents__hamburger--open');
+  })
 });
