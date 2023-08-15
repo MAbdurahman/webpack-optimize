f2a0ee3a4b1d5b43e9b52b54bfa9d719e5a1fecc
MAbdurahman <mdbdrrhm@gmail.com>
2023-08-15 06:33:47 -0500

    Add eventListeners window scroll and navbar_navigation and hamburger click

--- src/js/index.js
+++ src/js/index.js
@@ -35,5 +35,26 @@ $(function () {
 		navbar_navigation.classList.toggle('header__navbar__contents__nav--open');
 	});
 
-  
+  navbar_navigation.addEventListener('click', function() {
+    navbar_navigation.classList.remove('header__navbar__contents__nav--open');
+    hamburger.classList.remove('header__navbar__contents__hamburger--open');
+  })
+
+  window.addEventListener('scroll', function () {
+    sections.forEach(section => {
+      let top = window.scrollY;
+      let offset = section.offsetTop - 200;
+      let height = section.offsetHeight;
+      let id = section.getAttribute('id');
+
+      if ( (top >= offset) && (top < (offset + height)) ) {
+        list_links.forEach(list_link => {
+          list_link.classList.remove('active');
+          document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
+        })
+      }
+    })
+  })
+
+
 });
