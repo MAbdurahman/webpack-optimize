5ac8eeb15e3ddf361ea8537e472a03f8d02e43ac
MAbdurahman <mdbdrrhm@gmail.com>
2023-08-15 06:02:57 -0500

    Update website title and header__hero__title

--- src/js/index.js
+++ src/js/index.js
@@ -5,22 +5,35 @@ import './../styles/index.scss';
             preloader
 ================================================*/
 $(window).on('load', function () {
-  // makes sure that whole site is loaded
-  $('#preloader-gif, #preloader').fadeOut(8000, function() {});
+	// makes sure that whole site is loaded
+	$('#preloader-gif, #preloader').fadeOut(8000, function () {});
 });
 
 /* =============================================
             header and navigation
 ================================================*/
 $(function () {
-  console.log('document is ready!');
-  const navbar_navigation = document.querySelector('.header__navbar__contents__nav');
-  const hamburger = document.querySelector('.header__navbar__contents__hamburger');
-  console.log(navbar_navigation)
-  console.log(hamburger)
+	console.log('document is ready!');
+	const navbar_navigation = document.querySelector(
+		'.header__navbar__contents__nav'
+	);
+	const hamburger = document.querySelector(
+		'.header__navbar__contents__hamburger'
+	);
+	const sections = document.querySelectorAll('.js-section');
+	const list_links = document.querySelectorAll(
+		'.header__navbar__contents__nav__list__item__link'
+	);
 
-  hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('header__navbar__contents__hamburger--open');
-    navbar_navigation.classList.toggle('header__navbar__contents__nav--open');
-  })
+	console.log(navbar_navigation);
+	console.log(hamburger);
+	console.log(sections);
+	console.log(list_links);
+
+	hamburger.addEventListener('click', function () {
+		hamburger.classList.toggle('header__navbar__contents__hamburger--open');
+		navbar_navigation.classList.toggle('header__navbar__contents__nav--open');
+	});
+
+  
 });
